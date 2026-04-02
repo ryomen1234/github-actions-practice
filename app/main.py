15d93af7ce235b6d1bad8a579caea50922063093
harshal_kusalkar <h41497254@gmail.com>
2026-04-02 23:55:11 +0530

    Testing github-actions 2

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"message": "Hello from FastAPI 🚀"}
+    return {"message": "Hello from FastAPI 🚀\n This will now demostrate the real power of github actions"}
 
 @app.get("/predict")
 def predict(x: int):
